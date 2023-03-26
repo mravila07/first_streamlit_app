800957237b9077ff7b6ab81ae3d91242197f2f2b
mravila07 <128983127+mravila07@users.noreply.github.com>
2023-03-27 07:16:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_selected)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
